72ad688b2347dda9ab8c8963c8ed55a8e0a9045e
Saurabh Jadli <86057535+SaurabhJadli@users.noreply.github.com>
2024-10-14 22:44:32 +0530

    Add files via upload
    javas 1st main

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        int count = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            count += i;
+        }
+
+        System.out.println(count);
+    }
+
+}
